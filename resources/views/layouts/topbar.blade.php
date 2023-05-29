8a498e8af959d3b48ecd2f087b97c3651062d310
donaldmashiri <donaldtondemashiri@gmail.com>
2023-05-29 13:12:24 +0200

    footer corrected

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -96,7 +96,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                          <span>Copyright © Your Website 2023 developed by Calton Mukahiwa (R1910409A)</span>
+                          <span>Copyright © Your Website 2023 developed by Batsi</span>
                     </div>
                 </div>
             </footer>
