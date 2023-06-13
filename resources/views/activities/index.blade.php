668f1cb64500e0bc5fafc1159610314086aa25c6
donaldmashiri <donaldtondemashiri@gmail.com>
2023-06-13 12:46:27 +0200

    btn back

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -58,11 +58,12 @@
                                         <th>{{ $task->created_at}}</th>
                                         <th>
                                             @foreach ($task->activities as $activity)
-                                                @if ($activity->status !== 'Delivered')
                                                 <a href="{{ route('activities.show', $task->id)}}" class="btn btn-primary btn-sm">Start Task</a>
-                                                @else
-                                                    <p class="text-info font-weight-bolder">{{ $activity->status }}</p>
-                                                @endif
+{{--                                                @if ($activity->status !== 'Delivered')--}}
+{{--                                                    <a href="{{ route('activities.show', $task->id)}}" class="btn btn-primary btn-sm">Start Task</a>--}}
+{{--                                                @else--}}
+{{--                                                    <p class="text-info font-weight-bolder">{{ $activity->status }}</p>--}}
+{{--                                                @endif--}}
 
                                             @endforeach
                                         </th>
