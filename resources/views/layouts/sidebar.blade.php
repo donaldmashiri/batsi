0df7fdedf23afae15c6fa140d571d60ac4e350a9
donaldmashiri <donaldtondemashiri@gmail.com>
2023-06-13 09:54:09 +0200

    user

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
     <a class="nav-link collapsed " href="/profile"><i class="fas fa-fw fa-user"></i>Profile   </a>
 </li>
 
-{{--@if(Auth::user()->role === "admin")--}}
+@if(Auth::user()->role === "admin")
 <li class="nav-item ">
     <a class="nav-link collapsed " href="{{ route('users.index') }}"><i class="fas fa-fw fa-car"></i>Drivers</a>
 </li>
@@ -16,32 +16,11 @@
     <a class="nav-link collapsed " href="/reports"><i class="fas fa-fw fa-folder"></i>Reports</a>
 </li>
 
-{{--@else--}}
+@else
     <li class="nav-item ">
         <a class="nav-link collapsed " href="{{ route('activities.index') }}"><i class="fas fa-fw fa-landmark"></i>Activity</a>
     </li>
-{{--@endif--}}
-
-
-
-{{--@if(Auth::user()->role === "admin")--}}
-
-{{--    <li class="nav-item ">--}}
-{{--        <a class="nav-link collapsed " href="{{ route('markings.index') }}"><i class="fas fa-fw fa-marker"></i>Colors Markings</a>--}}
-{{--    </li>--}}
-{{--    <li class="nav-item ">--}}
-{{--        <a class="nav-link collapsed " href="/reports"><i class="fas fa-fw fa-file-download"></i>Reports</a>--}}
-{{--    </li>--}}
-{{--@else--}}
-{{--    <li class="nav-item ">--}}
-{{--        <a class="nav-link collapsed " href="{{ route('detections.index') }}"><i class="fas fa-fw fa-comment"></i>Camera Detections</a>--}}
-{{--    </li>--}}
-{{--    <li class="nav-item ">--}}
-{{--        <a class="nav-link collapsed " href="{{ route('videodetects.index') }}"><i class="fas fa-fw fa-comment"></i>Video Detections</a>--}}
-{{--    </li>--}}
-{{--@endif--}}
-
-
+@endif
 
 
 <li class="nav-item ">
